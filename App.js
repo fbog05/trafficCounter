c91a85d6d148770fcc4bf9f40588db78f73a8423
Ferencsik Boglárka <boglarkaferencsik05@gmail.com>
2024-02-14 12:09:22 +0100

    02141209

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import MainScreen from './components/MainScreen';
+import OperatorScreen from './components/OperatorScreen';
+import SaveScreen from './components/SaveScreen';
 
+const Tab = createMaterialTopTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Főoldal" component={MainScreen} />
+        <Tab.Screen name="Beállítások" component={OperatorScreen} />
+        <Tab.Screen name="Mentés" component={SaveScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
